5a6de7c87935e16e085dce9c8d0fa133b4d50fd4
AlejandroPenagos84 <alejopenagos79@gmail.com>
2024-08-23 18:46:51 -0500

    xd

---
+++ Pilas_Colas/EjercicioPilas.py
@@ -0,0 +1,5 @@
+from Estructuras_Datos import Pila
+
+def equilibrar_simbolos(x):
+
+    return x
--- Pilas_Colas/Estructuras_Datos.py
+++ Pilas_Colas/Estructuras_Datos.py
@@ -1,5 +1,3 @@
-'''
-Esta es la forma normalita de hacerlo
 class Pila():
     def __init__(self):
         self.items = []
@@ -42,44 +40,4 @@ class Cola():
 
     def tam(self):
         return len(self.items)
-'''
-from ast import Index
 
-'''
-En este enfoque la cola hereda de pila los elementos que son exactamente iguales como agregar, esta vacio,
-el constructor, etc. Los que cambian se sobrescriben
-
-class Pila():
-    def __init__(self):
-        self.items = []
-
-    def agregar(self,item):
-        self.items.append(item)
-
-    def remover(self):
-        if self.esta_vacia():
-            raise IndexError("La pila esta vacia")
-        return self.items.pop()
-
-    def top(self):
-        if self.esta_vacia():
-            raise IndexError("La pila esta vacia")
-        return self.items[-1]
-
-    def esta_vacia(self):
-        return self.items == []
-
-class Cola(Pila):
-    def remover(self):
-        if self.esta_vacia():
-            raise IndexError("La cola esta vacia")
-        return self.items.pop(0)
-
-    def top(self):
-        if self.esta_vacia():
-            raise IndexError("La cola esta vacia")
-        return self.items[0]
-
-    def tam(self):
-        return len(self.items)
-'''
